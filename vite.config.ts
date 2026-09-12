23f3871d0355510e5b275ae7db774e2025dc140f
flutterremote1 <flutterremote1@zamzamit.net>
2026-09-12 11:24:00 +0600

    build: fix baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://dakpion-server-n2gd.onrender.com/',
         changeOrigin: true,
       },
     },
